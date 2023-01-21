7791350275a71af8af7124c1fcc6f36443e5e7a4
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-01-21 14:48:40 -0800

    Finish react app demo

--- examples/react/tailwind.config.cjs
+++ examples/react/tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Work Sans', 'Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 }
